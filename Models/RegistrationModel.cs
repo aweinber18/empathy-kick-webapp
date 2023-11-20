d3a51dc48ad3f92619ca4d197079a6e58fa8232c
Avi Weinberg <aweinber18@student.touro.edu>
2023-11-20 18:44:29 -0500

    registration model and empty dbcontext

---
+++ Models/RegistrationModel.cs
@@ -0,0 +1,20 @@
+﻿namespace EmpathyKick.Models
+{
+    public class RegistrationModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string FullAddress { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Password { get; set; }
+        public string ThemeColor { get; set; }
+        public bool IsEmpathyAdmin { get; set; }
+        public bool IsOrganizationAdmin { get; set; }
+    }
+}
