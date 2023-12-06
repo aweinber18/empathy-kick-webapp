35192c64977f29e4d72f32cbfa9e02c833913832
Avi Weinberg <aweinber18@student.touro.edu>
2023-12-05 19:42:28 -0500

    sql queries formatted

--- Data/LocalDbContext.cs
+++ Data/LocalDbContext.cs
@@ -8,11 +8,12 @@ namespace EmpathyKick.Data
     {
         //Database database = new Database();
         public IList<string> GetTableNames() {
-            /*FormattableString sql = FormattableStringFactory.Create($"SELECT table_name " +
+            FormattableString sql = FormattableStringFactory.Create($"SELECT table_name " +
                                                         $"FROM information_schema.tables" +
                                                         $"WHERE TABLE_NAME NOT LIKE 'spt%' " +
                                                         $"AND NOT LIKE 'MSreplication%';");
-            var tables = _context.Tables.FromSqlRaw(sql.Format, sql.GetArguments()).ToList();*/
+
+            /*var tables = _context.Tables.FromSqlRaw(sql.Format, sql.GetArguments()).ToList();*/
 
             return new List<string> { "User", "Address", "EmpathyAdmin", "Organization", "OrganizationAdmin",
                 "Card", "CardRegistration", "Tag", "TagRegistration", "Donation", "Invoice", "Product"};
@@ -22,15 +23,20 @@ namespace EmpathyKick.Data
             var tableStringBuilder = new StringBuilder();
             for (int i = 0; i < tables.Count; i++)
             {
+                tableStringBuilder.Append("'");
                 tableStringBuilder.Append(tables[i]);
+                tableStringBuilder.Append("'");
+
                 if (i < tables.Count - 1) 
                     tableStringBuilder.Append(", ");
             }
-
-            /*FormattableString sql = FormattableStringFactory.Create($"SELECT COLUMN_NAME " +
-                                                        $"FROM information_schema.COLUMNS" +
-                                                        $"WHERE TABLE_NAME IN ;");
-            var tables = _context.Tables.FromSqlRaw(sql.Format, sql.GetArguments()).ToList();*/
+            string tablesString = "(" + tableStringBuilder.ToString() + ")";
+            FormattableString sql = FormattableStringFactory.Create($"SELECT COLUMN_NAME " +
+                                                        $"FROM information_schema.COLUMNS " +
+                                                        $"WHERE TABLE_NAME IN " + 
+                                                        tablesString + 
+                                                        ";");
+            /*var tables = _context.Tables.FromSqlRaw(sql.Format, sql.GetArguments()).ToList();*/
             return new List<string> { "AddressID", "Address", "City", "Region", "Country", "ZIP" };
         }
     }
