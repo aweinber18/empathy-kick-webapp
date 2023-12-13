72e620021f480b1e10cdbd48958d2abdda023f4a
Shlomo Kalinsky <93456838+sdkkds2125@users.noreply.github.com>
2023-12-13 18:55:56 -0500

    separated the Entity objects out of the MyDBContext class into their own classes

---
+++ Models/Donations.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmpathyKick.Models
+{
+    public class Donations
+    {
+        [Key]
+        public int ReceiptNumber { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int OrganizationId { get; set; }
+        public string DonorFullName { get; set; }
+        public int Amount { get; set; }
+        public string? PaymentMethod { get; set; }
+        public int? UserId { get; set; }
+        public int? CardId { get; set; }
+        public string? Frequency { get; set; }
+
+    }
+}
