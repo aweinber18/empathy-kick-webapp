35c5f0daad7b7887033517b98e604ae165f44e62
Shlomo Kalinsky <93456838+sdkkds2125@users.noreply.github.com>
2023-12-21 13:25:33 -0500

    added processing to take the passed in ids of the organizations and make a new tentative OA for this user and that OrganizationId

--- Models/OrganizationAdmin.cs
+++ Models/OrganizationAdmin.cs
@@ -1,15 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmpathyKick.Models
 {
+    [PrimaryKey(nameof(UserID), nameof(OrganizationID))]
     public class OrganizationAdmin
     {
-        [Key]
+     
+        [Column(Order = 0)] // Order is important for composite keys
         [ForeignKey("User")]
         public int UserID { get; set; }
 
-        [Key]
+       
+        [Column(Order = 1)] // Order is important for composite keys
         [ForeignKey("Organization")]
         public int OrganizationID { get; set; }
         public DateTime? AuthorizationDate { get; set; }
